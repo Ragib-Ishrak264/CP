2c5070c6a666260a14e061b597452ed0227e8807
Ragib-Ishrak264 <ragibishrak264@gmail.com>
2025-08-01 21:10:09 +0600

    Add files via upload

---
+++ Petercode1/c1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+   int a,b,c,d,x,r;
+   scanf("%d %d %d %d",&a,&b,&c,&d);
+    x= a*b*c*d;
+    r=x%100;
+
+    printf("%d",r);
+
+
+
+
+
+    return 0;
+}
---
+++ Petercode1/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cmath>
+
+
+using namespace std;
+
+int getMax(int num1, int num2 ){
+    int result;
+
+    if(num1 == num2){
+            result = num1;
+
+    }else{
+           char result1;
+           result1 = "not a match";
+    }
+
+
+
+    return result;
+}
+
+int main()
+{
+
+
+    cout << getMax(10,5);
+
+    return 0;
+
+
+}
+
+
